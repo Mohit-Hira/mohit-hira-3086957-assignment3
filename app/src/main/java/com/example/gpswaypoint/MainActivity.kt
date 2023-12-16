32790be79eace796c63b8841ff39a37eb2edf87e
Mohit <mohithira555@gmail.com>
2023-12-16 21:23:34 +0500

    Mohit <mohithira555@gmail.com>

--- app/src/main/java/com/example/gpswaypoint/MainActivity.kt
+++ app/src/main/java/com/example/gpswaypoint/MainActivity.kt
@@ -1,16 +1,139 @@
 package com.example.gpswaypoint
 
-
+import android.Manifest
+import android.content.Context
+import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.gson.Gson
+import java.io.File
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 
+class MainActivity : ComponentActivity(),SensorEventListener {
 
-class MainActivity : ComponentActivity(){
+    private lateinit var sensorManager: SensorManager
+    private var compassRotation = mutableStateOf(0f)
+    private lateinit var locationManager: LocationManager
+    private var waypoints = mutableListOf<Location>()
+    private val currentLocation = mutableStateOf<Location?>(null)
+    private val locationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            currentLocation.value = location
+
+        }
+
+        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+
+        // Registering the rotation vector sensor
+        val rotationVectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
+        sensorManager.registerListener(this, rotationVectorSensor, SensorManager.SENSOR_DELAY_NORMAL)
+        setContent {
+
+            MaterialTheme {
+                Surface(modifier = Modifier.fillMaxSize(),color = MaterialTheme.colors.background) {
+
+                }
+            }
+        }
 
     }
+    private fun checkProximityToCurrentWaypoint(currentLocation: Location) {
+        val currentSelectedIndex = waypoints.indexOf(selectedWaypoint.value)
+        selectedWaypoint.value?.let { currentWaypoint ->
+            if (currentLocation.distanceTo(currentWaypoint) < 10) {
+                // Check if there is a previous waypoint
+                if (currentSelectedIndex > 0) {
+                    selectedWaypoint.value = waypoints[currentSelectedIndex -1]
+                }
+            }
+        }
+    }
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event?.sensor?.type == Sensor.TYPE_ROTATION_VECTOR) {
+            // Update compassRotation based on the sensor data
+            val rotationMatrix = FloatArray(9)
+            SensorManager.getRotationMatrixFromVector(rotationMatrix, event.values)
+            val orientationAngles = FloatArray(3)
+            SensorManager.getOrientation(rotationMatrix, orientationAngles)
+            compassRotation.value = Math.toDegrees(orientationAngles[0].toDouble()).toFloat()
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // Handle sensor accuracy changes if needed
+    }
+    private var selectedWaypoint = mutableStateOf<Location?>(null)
+
+    private fun startTracking() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+            return
+        }
+        setupLocationTracking()
+    }
+
+    private fun setupLocationTracking() {
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            return
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0f, locationListener)
+    }
+
+    private fun stopTracking() {
+
+        if (this::locationManager.isInitialized) {
+            locationManager.removeUpdates(locationListener)
+        }
+    }
+
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1 && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            setupLocationTracking()
+        }
+    }
 }
-
-
--- app/src/main/java/com/example/gpswaypoint/SharedComposables.kt
+++ app/src/main/java/com/example/gpswaypoint/SharedComposables.kt
@@ -1,12 +1,126 @@
 package com.example.gpswaypoint
 
+import android.graphics.Paint
+import android.graphics.Typeface
+import android.location.Location
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.AlertDialog
+
+import androidx.compose.material3.*
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.withTransform
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.lang.Math.atan2
+import java.lang.Math.cos
+import java.lang.Math.log
+import java.lang.Math.sin
+import java.lang.Math.tan
+
 
 @Composable
-fun CompassCanvas() {
+fun CompassCanvas(waypoints: List<Location>, currentLocation: Location?, rotation: Float
+                  , selectedWaypoint: Location?) {
     Canvas(modifier = Modifier.aspectRatio(1f)) {
+        val centerX = size.width / 2
+        val centerY = size.height / 2
+        val radius = size.minDimension / 2
+
+        // Draw the compass circle
+        drawCircle(
+            color = Color.Black,
+            radius = radius,
+            center = Offset(centerX, centerY)
+        )
+        drawCircle(
+            color = Color.White,
+            radius = radius-2,
+            center = Offset(centerX, centerY)
+        )
+        val textPaint = Paint().apply {
+            textAlign = Paint.Align.CENTER
+            textSize = 40f
+            typeface = Typeface.DEFAULT_BOLD
+        }
+        // Apply rotation to the canvas
+        withTransform({
+            // Apply rotation to the canvas
+            rotate(rotation, pivot = Offset(centerX, centerY))
+        }) {
+            val textPaint = Paint().apply {
+                textAlign = Paint.Align.CENTER
+                textSize = 40f
+                typeface = Typeface.DEFAULT_BOLD
+            }
+
+            // Draw the compass directions
+            // Drawing North
+            textPaint.color = Color.Red.toArgb()
+            drawContext.canvas.nativeCanvas.drawText("N", centerX, centerY - radius + 20, textPaint)
+
+            // Drawing East
+            textPaint.color = Color.Black.toArgb()
+            drawContext.canvas.nativeCanvas.drawText("E", centerX + radius - 20, centerY, textPaint)
+
+            // Drawing West
+            drawContext.canvas.nativeCanvas.drawText("W", centerX - radius + 20, centerY, textPaint)
+
+            // Drawing South
+            drawContext.canvas.nativeCanvas.drawText("S", centerX, centerY + radius - 20, textPaint)
+        }
+        // Draw lines pointing towards each waypoint
+
+
     }
 }
+fun bearingTo(start: Location, end: Location): Float {
+    val startLat = Math.toRadians(start.latitude)
+    val startLong = Math.toRadians(start.longitude)
+    val endLat = Math.toRadians(end.latitude)
+    val endLong = Math.toRadians(end.longitude)
+
+    val dLong = endLong - startLong
+    val dPhi = log(tan(endLat / 2.0 + Math.PI / 4.0) / tan(startLat / 2.0 + Math.PI / 4.0))
+
+    return (Math.toDegrees(atan2(dLong, dPhi)).toFloat() + 360.0f) % 360.0f
+}
+
+
+
+
+
+
