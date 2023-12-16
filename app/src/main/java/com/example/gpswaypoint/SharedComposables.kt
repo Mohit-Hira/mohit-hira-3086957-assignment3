1cbd47d12c533510610a00ec15605701030e06f2
Mohit <mohithira555@gmail.com>
2023-12-16 20:37:29 +0500

    Mohit <mohithira555@gmail.com>

---
+++ app/src/main/java/com/example/gpswaypoint/SharedComposables.kt
@@ -0,0 +1,12 @@
+package com.example.gpswaypoint
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CompassCanvas() {
+    Canvas(modifier = Modifier.aspectRatio(1f)) {
+    }
+}
